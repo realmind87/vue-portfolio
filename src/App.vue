3330693da1abecdceaefe6ec50906bc082302cb5
kim dong young <realmind@naver.com>
2021-05-05 20:15:42 +0900

    210505 commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,91 @@
 <template>
   <div id="app">
-	<h1>App test</h1>
+    <div id="wrap">
+      <div class='container'>
+        <div id="content">
+          <List v-bind:portfolio="portfolios"></List>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
+import List from "@/views/list/List"
 export default {
   name: 'App',
+  data(){
+    return {
+      portfolios : [
+        {
+          id:0,
+          name:'구축 1',
+          type:'build',
+          url:'www.naver.com',
+          date:{
+            start:202001,
+            end:202002,
+          }
+        },
+        {
+          id:1,
+          name:'운영 2',
+          type:'manage',
+          url:'www.naver.com',
+          date:{
+            start:202003,
+            end:202004,
+          }
+        },
+        {
+          id:2,
+          name:'구축 3',
+          type:'build',
+          url:'www.naver.com',
+          date:{
+            start:202003,
+            end:202004,
+          }
+        },
+        {
+          id:3,
+          name:'구축 4',
+          type:'build',
+          url:'www.naver.com',
+          date:{
+            start:202003,
+            end:202004,
+          }
+        },
+        {
+          id:4,
+          name:'운영 5',
+          type:'manage',
+          url:'www.naver.com',
+          date:{
+            start:202003,
+            end:202004,
+          }
+        },
+        {
+          id:5,
+          name:'구축 6',
+          type:'build',
+          url:'www.naver.com',
+          date:{
+            start:202003,
+            end:202004,
+          }
+        }
+      ]
+    }
+  },
+  components : {
+    List
+  }
 }
 </script>
 
 <style>
+  @import url('assets/css/reset.css');
+  @import url('assets/css/layout.css');
 </style>
---
+++ src/views/list/List.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list">
+        <ul>
+            <li v-for="(project , index) in portfolio" :key="index">
+                <a href="#">{{ project.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"list",
+    props:['portfolio']
+}
+</script>
+
+<style scoped>
+    .list {}
+    .list ul {display: flex; flex-wrap: wrap;}
+    .list ul li {flex-shrink: 0;  width:160px;margin:10px;}
+    .list ul li a {display: block;width:100%;height:160px;font-size:1rem;background-color:#fff;}
+</style>
