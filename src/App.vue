45aa768fd2575ac1a465a2de59bba11a9464fed5
kim dong young <realmind@naver.com>
2021-05-07 00:57:56 +0900

    portfoilo vue

--- src/App.vue
+++ src/App.vue
@@ -10,77 +10,20 @@
   </div>
 </template>
 <script>
+
+import Data from '../database/data.json'
 import List from "@/views/list/List"
 export default {
   name: 'App',
   data(){
     return {
-      portfolios : [
-        {
-          id:0,
-          name:'구축 1',
-          type:'build',
-          url:'www.naver.com',
-          date:{
-            start:202001,
-            end:202002,
-          }
-        },
-        {
-          id:1,
-          name:'운영 2',
-          type:'manage',
-          url:'www.naver.com',
-          date:{
-            start:202003,
-            end:202004,
-          }
-        },
-        {
-          id:2,
-          name:'구축 3',
-          type:'build',
-          url:'www.naver.com',
-          date:{
-            start:202003,
-            end:202004,
-          }
-        },
-        {
-          id:3,
-          name:'구축 4',
-          type:'build',
-          url:'www.naver.com',
-          date:{
-            start:202003,
-            end:202004,
-          }
-        },
-        {
-          id:4,
-          name:'운영 5',
-          type:'manage',
-          url:'www.naver.com',
-          date:{
-            start:202003,
-            end:202004,
-          }
-        },
-        {
-          id:5,
-          name:'구축 6',
-          type:'build',
-          url:'www.naver.com',
-          date:{
-            start:202003,
-            end:202004,
-          }
-        }
-      ]
+      portfolios : Data
     }
   },
   components : {
     List
+  },
+  created(){
   }
 }
 </script>
--- src/views/list/List.vue
+++ src/views/list/List.vue
@@ -1,23 +1,33 @@
 <template>
     <div class="list">
-        <ul>
+        <transition-group name="project-list" tag="ul">
             <li v-for="(project , index) in portfolio" :key="index">
-                <a href="#">{{ project.name }}</a>
+                <a href="#">{{ project.project_name }}</a>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
 <script>
+
+const onResize = () => {
+    console.log( 'resize' );
+}
+
 export default {
     name:"list",
-    props:['portfolio']
+    props:['portfolio'],
+    mounted(){
+        window.addEventListener('resize', onResize);
+    },
+    methods : {
+    }
 }
 </script>
 
 <style scoped>
-    .list {}
-    .list ul {display: flex; flex-wrap: wrap;}
-    .list ul li {flex-shrink: 0;  width:160px;margin:10px;}
-    .list ul li a {display: block;width:100%;height:160px;font-size:1rem;background-color:#fff;}
+    .list {text-align: center;}
+    .list ul {width:100%;max-width:900px;display:inline-block;text-align:left;}
+    .list ul li {display:inline-block;width:160px;margin:10px;vertical-align:middle;}
+    .list ul li a {display:block;width:100%;height:160px;font-size:1.6rem;background-color:#fff;}
 </style>
