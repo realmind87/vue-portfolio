644f9a6d69f9d584d0b07f4b9bd50c0407f7bc4b
kim dong young <realmind@naver.com>
2021-05-14 00:52:04 +0900

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class='header' :style="style">
+    <header class='header' :class="{detailview:detailview}">
         <div class='inner'>
-            <h1 :style="translatetop">{{ headertitle }}</h1>
+            <h1>{{ headertitle }}</h1>
             <transition name='navi'>
                 <div class='gnb' v-if="isNavigation">
                     <nav>
@@ -25,31 +25,43 @@ export default {
             if(this.headerdata == null) return 'Portfolio'
             else return this.headerdata.project_name
         },
-        style(){
-            if(this.headerdata != null) return {height:200+'px'}
-            else return false 
-        },
-        translatetop(){
-            if(this.headerdata != null) return {transform:`translate3d(${0},${100}px,${0})`}
-            else return false;
-        },
         isNavigation(){
             if(this.headerdata != null) return false
             else return true;
+        },
+        detailview(){
+            if(this.headerdata != null) return true
+            else return false;
         }
     }
 }
 </script>
 
 <style scoped>
-    .header {position:relative;top:0px;left:0px;width:100%;height:80px;transition:all 0.5s ease;}
-    .header .inner {overflow:hidden;padding-top:50px;width:100%;max-width:1160px;height:100%;margin:0 auto;box-sizing: border-box;}
-    .header h1 {float:left; font-size:3rem;color:#333;transform:translate3d(0,0,0);transition:all 0.5s ease;}
-    .header .gnb {float:right;}
+
+    /* mobile */
+    .header {position:relative;width:100%;height:64px;background-color:#333;transition:all 0.5s ease;}
+    .header .inner {position:relative;padding:20px;width:100%;max-width:1160px;height:100%;margin:0 auto;box-sizing: border-box;}
+    .header h1 {font-size:2.4rem;color:#fff;transform:translate3d(0,0,0);transition:all 0.5s ease;}
+    .header .gnb {display:none;}
     .header .gnb li {display:inline-block;}
-    .header .gnb li a {display:block;padding:0 10px;font-size:2rem;color:#333;}
-    .navi-enter-active, 
-    .navi-leave-active {transition: all .5s ease;}
-    .navi-enter, 
-    .navi-leave-to {transform:translate3d(0,100px,0);opacity: 0;}
+    .header .gnb li a {display:block;padding:5px 10px;font-size:1.6rem;color:#fff;}
+    
+    .header.detailview {background-color:transparent;height:80px;}
+    .header.detailview h1 {color:#333;transform:translate3d(0,15px,0);}
+    
+    @media screen and (min-width: 550px) {
+        .header {height:100px;background-color:transparent;}
+        .header .inner {padding:60px 0px 0px;}
+        .header h1 {color:#333;}
+        .header .gnb {display:block;position:absolute;right:0px;top:60px;}
+        .header .gnb li a {color:#333;}
+        .header.detailview {background-color:transparent;height:140px;}
+        .header.detailview h1 {color:#333;transform:translate3d(0,40px,0);}
+        .header .navi-enter-active, 
+        .header .navi-leave-active {transition: all .5s ease;}
+        .header .navi-enter, 
+        .header .navi-leave-to {transform:translate3d(0,40px,0);opacity: 0;}
+    }
+
 </style>
--- src/views/layouts/detail/Detail.vue
+++ src/views/layouts/detail/Detail.vue
@@ -9,12 +9,10 @@
                 </div>
 
                 <div class="col-area">
-                    <strong class="tit">DATE</strong>
                     <span class="data-txt">{{ portfolio.date }}</span>
                 </div>
 
                 <div class="col-area">
-                    <strong class="tit">LINK</strong>
                     <a v-if="btnlink" class='btn-link' :title="linktitle" target="_blank" :href="portfolio.url">링크</a>
                     <span v-else class='none-link'>링크 없음</span>
                 </div>
@@ -56,15 +54,20 @@ export default {
 </script>
 
 <style scoped>
-    .detail-wrapper {position: relative;width:100%;max-width:1160px;display:block;margin:0 auto;padding:30px;border-radius:10px;background-color:#fff;font-size:1.6rem;box-sizing:border-box;}
+    .detail-wrapper {position: relative;width:100%;max-width:1160px;display:block;margin:0 auto;padding:20px;border-radius:10px;background-color:#fff;font-size:1.6rem;box-sizing:border-box;}
     .detail-wrapper h2 {font-size:2rem;}
     .detail-wrapper .btn-go-list {display:block;width:70px;height:40px;text-align:center;line-height:43px;background-color:#1473e6;color:#fff;font-size:1.6rem;}
-    .detail-wrapper .site-img-area {width:100%;height:500px;background-color:#757575;}
+    .detail-wrapper .site-img-area {width:100%;height:300px;background-color:#757575;}
     .btn-go-list {display:block;width:70px;height:40px;text-align:center;line-height:43px;background-color:#1473e6;color:#fff;}
-    .detail-content {margin-top:30px;}
+    .detail-content {margin-top:20px;}
     .col-area {padding:10px 0px;}
     .col-area .tit {margin-bottom:5px;font-weight:normal;font-size:3rem;}
     .col-area .btn-link {display:block;width:70px;height:40px;text-align:center;line-height:43px;background-color:#1473e6;color:#fff;font-size:1.6rem;}
     .col-area .none-link {display:block;width:70px;height:40px;text-align:center;line-height:43px;background-color:#efefef;color:#333;font-size:1.6rem;}
     .col-area .data-txt {display:inline-block;margin-left:5px;font-size:1.8rem;color:#757575;}
+    
+    @media screen and (min-width: 550px) {
+        .detail-wrapper {padding:40px;}
+        .detail-wrapper .site-img-area {height: 400px;}
+    }
 </style>
